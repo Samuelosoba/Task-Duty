a147bc99bd79cbc5a9cb64fc49fbb7a690268bb1
osobasamuel <samuelosoba7@gmail.com>
2025-05-11 04:01:14 +0100

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 
 export default function App() {
-  return (
-    <div className="bg-white">
-      <AppRoutes />
-    </div>
-  );
+  return <AppRoutes />;
 }
