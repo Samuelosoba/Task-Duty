c672fc63d397abede1fea47680a46af33e875486
osobasamuel <samuelosoba7@gmail.com>
2025-05-11 04:04:04 +0100

    .

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-//  @type {import('tailwindcss').Config} 
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Add paths to your HTML/JSX/TSX files
-  darkMode: false, // Disable dark mode completely
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
